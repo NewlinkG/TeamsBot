f38b91d2d63b30d3e2090daec73a54a78c9ceb55
Alfred Vilsmeier <alfred_vilsmeier@hotmail.com>
2025-05-30 15:25:00 -0500

    Tab buttons and modal 2

--- teams-tab/src/index.js
+++ teams-tab/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import CommentModal from './CommentModal';
@@ -9,12 +9,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/comment" element={<CommentModal />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
